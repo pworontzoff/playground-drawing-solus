241d53147a34b68aa68d19678cd36fa15354a42c
pworontzoff <pierre.worontzoff@hepl.be>
2020-10-15 15:07:43 +0200

    Update main.c

--- c_drawing_4/main.c
+++ c_drawing_4/main.c
@@ -5,14 +5,31 @@
 #define WIDTH 800
 #define HEIGHT 600
 
+void polygone(int, int);
+void flower(int, int);
+
 int main() {
     init_drawing(WIDTH, HEIGHT, 0.5);
 
-    for (i=0;i<4;i++) {
-        draw(50);
-        turn(90,LEFT);
-    }
+    flower(50,10);
 
     display_drawing();
     return 0;
+}
+
+void flower(int length, int side) {
+    int i;
+    for (i=1;i<=side;i++) {
+        polygone(length,5);
+        draw(length);
+        turn(360/side,RIGHT);
+    }
+}
+
+void polygone(int length, int side) {
+    int i;
+    for (i=0;i<side;i++) {
+        draw(length);
+        turn(360/side,LEFT);
+    }
 }
