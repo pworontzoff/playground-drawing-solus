91f9146448e394eab813ce70dd3eb35f20741f39
pworontzoff <pierre.worontzoff@hepl.be>
2020-10-15 15:05:12 +0200

    Update main.c

--- c_drawing_2/main.c
+++ c_drawing_2/main.c
@@ -6,12 +6,27 @@
 #define HEIGHT 600
 
 int main() {
+    int taille = 100;
+    int taille2 = round(sqrt(taille*taille*2));
+    
     init_drawing(WIDTH, HEIGHT, 0.5);
-
-    for (i=0;i<4;i++) {
-        draw(50);
-        turn(90,LEFT);
-    }
+    
+    turn(90,LEFT);
+    draw(taille);
+    turn(30,RIGHT);
+    draw(taille);
+    turn(120,RIGHT);
+    draw(taille);
+    turn(30,RIGHT);
+    draw(taille);
+    turn(135,RIGHT);
+    draw(taille2);
+    turn(135,RIGHT);
+    draw(taille);
+    turn(135,RIGHT);
+    draw(taille2);
+    turn(135,LEFT);
+    draw(taille);
 
     display_drawing();
     return 0;
