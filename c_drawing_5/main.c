fe56f354b70bd5f741306c2115de6c07b0bf885c
pworontzoff <pierre.worontzoff@hepl.be>
2020-10-15 15:09:32 +0200

    Update main.c

--- c_drawing_5/main.c
+++ c_drawing_5/main.c
@@ -5,14 +5,42 @@
 #define WIDTH 800
 #define HEIGHT 600
 
+void polygone(int, int);
+void line_of_square(int, int);
+
 int main() {
+    int tour,i;
+    
     init_drawing(WIDTH, HEIGHT, 0.5);
 
-    for (i=0;i<4;i++) {
-        draw(50);
-        turn(90,LEFT);
-    }
+    turn(180,LEFT);
+    move(WIDTH/2-50);
+    turn(90,RIGHT);
+    move(HEIGHT/2-150);
+    turn(90,RIGHT);
+
+	for (i=0;i<5;i++) {
+    	line_of_square(15,20);
+    	turn(144,RIGHT);
+	}
 
     display_drawing();
     return 0;
+}
+
+void polygone(int length, int side) {
+    int i;
+    for (i=0;i<side;i++) {
+        draw(length);
+        turn(360/side,LEFT);
+    }
+}
+
+void line_of_square(int number, int length) {
+    int i;
+    
+    for (i=0;i<number;i++) {
+        polygone(length,4);
+        draw(length);   
+    }
 }
