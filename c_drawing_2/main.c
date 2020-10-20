21b844b09f2675f4789cd1c50648004b493333c7
pworontzoff <pierre.worontzoff@hepl.be>
2020-10-20 10:05:41 +0200

    Update main.c

--- c_drawing_2/main.c
+++ c_drawing_2/main.c
@@ -8,47 +8,6 @@
 int main() {
     int taille = 100;
     int taille2 = round(sqrt(taille*taille*2));
-    
-    init_drawing(WIDTH, HEIGHT, 0.5);
-    
-    // déplacement avant de démarrer le dessin :
-    turn(180,LEFT);
-    move(WIDTH/2-50);
-    turn(90,LEFT);
-    move(HEIGHT/2-20);
-    turn(90,LEFT);
-    
-    // dessiner :
-    turn(90,LEFT);
-    draw(taille);
-    turn(30,RIGHT);
-    draw(taille);
-    turn(120,RIGHT);
-    draw(taille);
-    turn(30,RIGHT);
-    draw(taille);
-    turn(135,RIGHT);
-    draw(taille2);
-    turn(135,RIGHT);
-    draw(taille);
-    turn(135,RIGHT);
-    draw(taille2);
-    turn(135,LEFT);
-    draw(taille);
-
-    display_drawing();
-    return 0;
-}
-#include <stdio.h>
-#include <stdlib.h>
-#include "drawing.h"
-
-#define WIDTH 200
-#define HEIGHT 220
-
-int main() {
-    int taille = 100;
-    int taille2 = round(sqrt(taille*taille*2));
     int i;
     
     init_drawing(WIDTH, HEIGHT, 0.5);
