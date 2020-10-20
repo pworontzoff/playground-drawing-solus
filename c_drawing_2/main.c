6bc160502df973a9192d7d9db172bb2ca8cfc0c5
pworontzoff <pierre.worontzoff@hepl.be>
2020-10-20 10:04:54 +0200

    Update main.c

--- c_drawing_2/main.c
+++ c_drawing_2/main.c
@@ -39,3 +39,49 @@ int main() {
     display_drawing();
     return 0;
 }
+#include <stdio.h>
+#include <stdlib.h>
+#include "drawing.h"
+
+#define WIDTH 200
+#define HEIGHT 220
+
+int main() {
+    int taille = 100;
+    int taille2 = round(sqrt(taille*taille*2));
+    int i;
+    
+    init_drawing(WIDTH, HEIGHT, 0.5);
+    
+    // déplacement avant de démarrer le dessin :
+    turn(180,LEFT);
+    move(WIDTH/2-50);
+    turn(90,LEFT);
+    move(HEIGHT/2-20);
+    turn(90,LEFT);
+    
+    // dessiner :
+    turn(90,LEFT);
+    draw(taille);
+    turn(30,RIGHT);
+    draw(taille);
+    turn(120,RIGHT);
+    draw(taille);
+    turn(30,RIGHT);
+    draw(taille);
+    for (i=0; i<4; i++) {
+        if (i==3) {
+            turn(135,LEFT);
+        } else {
+            turn(135,RIGHT);
+        }
+        if (i%2==0) {
+            draw(taille2);
+        } else {
+            draw(taille);
+        }
+    }
+
+    display_drawing();
+    return 0;
+}
